43ef9ac1ab2293270a6c1ecef3beb8ed248a5ab2
Tsabit <tsabittmn@gmail.com>
2024-05-01 21:02:07 +1000

    Calories data created

--- cs101PortfolioProject.py
+++ cs101PortfolioProject.py
@@ -1,6 +1,10 @@
-print("Hello there!")
-name = input("What's your name? ")
+# import necessary modules
+import pandas as pd
 
-print("Hello,", name)
-
-print("This is new!")
+# create the calPerAct dataset
+c = {'activities': ['sedentary', 'lightAct', 'moderateAct', 'highAct'], 
+     'cutting': [8, 10, 12, 14],
+     'leanGaining': [14, 16, 18, 20],
+     'maintaining': [12, 14, 16, 18]}
+df = pd.DataFrame(data=c)
+print(df)
