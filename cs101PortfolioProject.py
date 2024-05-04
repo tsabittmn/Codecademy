d55cdde93e98987be399cb3263ddc75ea2778686
Tsabit <tsabittmn@gmail.com>
2024-05-04 22:08:37 +1000

    datasets, prompt user

--- cs101PortfolioProject.py
+++ cs101PortfolioProject.py
@@ -6,11 +6,30 @@ c = {'activities': ['sedentary', 'lightAct', 'moderateAct', 'highAct'],
      'cutting': [8, 10, 12, 14],
      'leanGaining': [14, 16, 18, 20],
      'maintaining': [12, 14, 16, 18]}
-df = pd.DataFrame(data=c)
-# print(df)
+calPerAct = pd.DataFrame(data=c)
 
+# loading macros data
 location = "Codecademy/macros.csv"
 macros = pd.read_csv(location)
-print(macros[macros['food']=='apple', 3])
+
+# prompt user data
+weight = input("What is your weight(kg)?\n"
+               "Answer: ")
+actLevel = input("Please choose your current activity level:\n"
+                 "'a': little or no exercise\n"
+                 "'b': 1-4 hours of exercise per week\n"
+                 "'c': 5-8 hours of exercise per week\n"
+                 "'d': 9 or more hours of exercise per week\n"
+                 "Answer: ")
+fitGoal = input("Please choose your body composition goal:\n"
+                 "'a': cutting\n"
+                 "'b': lean gaining\n"
+                 "'c': maintaining\n"
+                 "Answer: ")
+
+
+
+
+
 
 
